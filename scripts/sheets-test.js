26158308142c059c373fe7a0f95d244dbbe4499a
Tom Hazledine <tom@tomhazledine.com>
2023-09-28 12:46:46 +0100

    parse sheet data and save as JSON

--- scripts/sheets-test.js
+++ scripts/sheets-test.js
@@ -1,5 +1,9 @@
 import google from "@googleapis/sheets";
+import path, { parse } from "path";
 import { config } from "dotenv";
+
+import { saveFile } from "./io.js";
+import { log } from "./console.js";
 
 config();
 
@@ -8,20 +12,36 @@ const sheets = google.sheets({
     auth: process.env.GOOGLE_API_KEY
 });
 
+log("Getting health data", "green");
+
 try {
+    log("· Reading Google Sheet...");
     const response = await sheets.spreadsheets.values.get({
         spreadsheetId: "10fWoH-E7CQQvd5MLT-GewFYo4EtWiUiKxhHunB5Povc",
         range: "Weight!A:D"
     });
+    log("· Parsing data...");
     const rows = response.data.values;
-    if (rows.length) {
-        rows.map(row => {
-            console.log(row.join(", "));
-        });
-    } else {
-        console.log("No data found.");
-    }
+    const [headers, ...rawValues] = rows;
+    log(`· Found ${rawValues.length} rows with ${headers.length} columns:`);
+    const headerList = headers.map(header => `"${header}"`).join(" | ");
+    log(`    ${headerList}`);
+    const values = rawValues.map(row => {
+        const [date, st_lb, st_percent, kg] = row;
+        return {
+            date,
+            st_lb,
+            st_percent: parseFloat(st_percent),
+            kg: parseFloat(kg)
+        };
+    });
+    log("· Saving data as JSON...");
+    saveFile(
+        path.resolve(process.cwd(), "./build/data.json"),
+        JSON.stringify(values)
+    );
+    log("Complete!", "green");
 } catch (err) {
-    console.log("Oops");
+    log("Oops", "red");
     console.error(err);
 }
