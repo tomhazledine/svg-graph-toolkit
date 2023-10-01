4c7d97af38224d1c49b33838dada4090bf406c72
Tom Hazledine <tom@tomhazledine.com>
2023-10-01 11:55:02 +0100

    GraphLine component

---
+++ src/js/components/graphs/GraphLine.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { slugify } from "../../utils.js";
+
+const GraphLine = ({
+    label = "",
+    className = "line",
+    orientation = "horizontal",
+    value,
+    bounds
+}) => {
+    const data =
+        orientation === "horizontal"
+            ? [bounds.left, value, bounds.right, value]
+            : [value, bounds.bottom, value, bounds.top];
+
+    return (
+        <line
+            className={`${className}__line ${className}__line--${slugify(
+                label
+            )}`}
+            x1={data[0]}
+            y1={data[1]}
+            x2={data[2]}
+            y2={data[3]}
+        />
+    );
+};
+
+export default GraphLine;
