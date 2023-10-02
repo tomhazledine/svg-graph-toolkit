cdaca0103f1749dde6f2b28a6863f13e6b3058d0
Tom Hazledine <tom@tomhazledine.com>
2023-10-02 17:24:14 +0100

    GraphBlock component

---
+++ src/js/components/graphs/GraphBlock.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { slugify } from "../../utils.js";
+
+const GraphBlock = ({
+    className = "test-graph",
+    scales,
+    x0,
+    x1,
+    y0,
+    y1,
+    label = ""
+}) => (
+    <rect
+        className={`${className}__block ${className}__block--${slugify(label)}`}
+        x={scales.x(x0)}
+        y={scales.y(y0)}
+        width={Math.abs(scales.x(x1) - scales.x(x0))}
+        height={Math.abs(scales.y(y1) - scales.y(y0))}
+    />
+);
+
+export default GraphBlock;
