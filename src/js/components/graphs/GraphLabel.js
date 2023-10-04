78c2934cf00c17e336bbc6d26a14bf8bed3c6e73
Tom Hazledine <tom@tomhazledine.com>
2023-10-04 11:34:18 +0100

    GraphLabel component

---
+++ src/js/components/graphs/GraphLabel.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const GraphLabel = ({ className, text, position, layout, buffer = 20 }) => {
+    const orientation = {
+        y: position.y > layout.graph.top + buffer ? "top" : "bottom",
+        x:
+            position.x > layout.graph.right - buffer
+                ? "right"
+                : position.x < layout.graph.left + buffer
+                ? "left"
+                : "middle"
+    };
+    return (
+        <text
+            className={`${className}__label`}
+            x={position.x}
+            y={position.y}
+            pointerEvents="none"
+            textAnchor={orientation.x}
+            dy={orientation.y === "top" ? "-0.6rem" : "1.2rem"}
+        >
+            {text}
+        </text>
+    );
+};
+
+export default GraphLabel;
